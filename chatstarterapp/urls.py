a83011dbff3c5118e25381b30d38aee31830267c
Sleptsov Oleg <slepcov_oleg@mail.ru>
2025-08-17 15:37:45 +0700

    Add apllication

---
+++ chatstarterapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import include, path
+
+from .views import (
+    InstallView,
+    IndexView
+)
+
+
+app_name = 'chatstarterapp'
+
+
+urlpatterns = [
+    path('index/', IndexView.as_view()),
+    path('install/', InstallView.as_view()),
+]
+
