130b99d8e8463aecf564619c08c3cb65a8d4a1cd
Sleptsov Oleg <slepcov_oleg@mail.ru>
2026-01-18 22:01:46 +0700

    Added new field of messanger selection

--- chatstarterapp/static/chatstarterapp/js/index.js
+++ chatstarterapp/static/chatstarterapp/js/index.js
@@ -36,9 +36,19 @@ function renderReceiver(fields) {
 }
 
 
+function renderSelectField(elementId, fields) {
+    let contentHTML = '';
+    for (const field of fields) {
+        contentHTML += `<option value="${field.ID}">${field.VALUE}</option>`;
+    }
+    const selectElem = document.getElementById(elementId);
+    if (selectElem) {
+        selectElem.innerHTML = contentHTML;
+    }
+}
+
+
 async function handleChatFromDeal(dealId) {
-    // UF_CRM_689C03C9DF905 - С кем создать чат
-    // UF_CRM_689C03C98BB2C - Менеджер создавший чат
     let cmd = {
         user: ['user.current', {}],
         fields: [`crm.deal.fields`, {}],
@@ -48,22 +58,27 @@ async function handleChatFromDeal(dealId) {
                 filter: { ID: dealId },
                 select: [
                     'ID',
-                    'UF_CRM_689C03C9DF905',
-                    'UF_CRM_689C03C98BB2C'
+                    'UF_CRM_689C03C9DF905',     // С кем нужен новый чат
+                    'UF_CRM_689C03C98BB2C',     // Менеджер, создавший чат
+                    'UF_CRM_69412104934C4',     // Месседжер для общения
                 ]
             }
         ]
     };
 
     const result = await callBatchPromise(cmd);
+
     const user = result.user.data();
     const fields = result.fields.data();
     const deal = result.deal.data()?.[0];
 
+
     console.log('user: ', user);
     console.log('fields: ', fields?.UF_CRM_689C03C9DF905?.items);
     console.log('deal: ', deal);
-    renderReceiver(fields?.UF_CRM_689C03C9DF905?.items);
+    // renderReceiver(fields?.UF_CRM_689C03C9DF905?.items);
+    renderSelectField('receiver', fields?.UF_CRM_69412104934C4?.items);
+    renderSelectField('messenger', fields?.UF_CRM_69412104934C4?.items);
 
     document.getElementById('createChatWA').addEventListener('click', async (event) => {
         const button = event.target;
@@ -72,9 +87,11 @@ async function handleChatFromDeal(dealId) {
 
         const manager = `${user?.LAST_NAME} ${user?.NAME}`;
         const receiverId = document.getElementById('receiver').value;
+        const messengerId = document.getElementById('messenger').value;
         const message = document.getElementById('message').value;
         console.log('manager: ', manager);
         console.log('receiverId: ', receiverId);
+        console.log('messengerId: ', messengerId);
         console.log('message: ', message);
         const resultUpdate = await callMethodPromise(
             'crm.deal.update',
@@ -82,12 +99,12 @@ async function handleChatFromDeal(dealId) {
                 id: dealId,
                 fields: {
                     UF_CRM_689C03C9DF905: receiverId,
-                    UF_CRM_689C03C98BB2C: manager
+                    UF_CRM_689C03C98BB2C: manager,
+                    UF_CRM_69412104934C4: messengerId
                 }
             }
         );
         console.log('resultUpdate = ', resultUpdate);
-
 
         const resultStartBizproc = await callMethodPromise(
             'bizproc.workflow.start',
@@ -125,8 +142,9 @@ async function handleChatFromLead(leadId) {
                 filter: { ID: leadId },
                 select: [
                     'ID',
-                    'UF_CRM_1755054223541',
-                    'UF_CRM_1755054174474'
+                    'UF_CRM_1755054223541',     // С кем нужен новый чат
+                    'UF_CRM_1755054174474',     // Менеджер, создавший чат
+                    'UF_CRM_1765866255',        // Месседжер для общения
                 ]
             }
         ]
@@ -140,7 +158,9 @@ async function handleChatFromLead(leadId) {
     console.log('user: ', user);
     console.log('fields: ', fields?.UF_CRM_1755054223541?.items);
     console.log('lead: ', lead);
-    renderReceiver(fields?.UF_CRM_1755054223541?.items);
+    // renderReceiver(fields?.UF_CRM_1755054223541?.items);
+    renderSelectField('receiver', fields?.UF_CRM_1755054223541?.items);
+    renderSelectField('messenger', fields?.UF_CRM_1765866255?.items);
 
     document.getElementById('createChatWA').addEventListener('click', async (event) => {
         const button = event.target;
@@ -149,9 +169,11 @@ async function handleChatFromLead(leadId) {
 
         const manager = `${user?.LAST_NAME} ${user?.NAME}`;
         const receiverId = document.getElementById('receiver').value;
+        const messengerId = document.getElementById('messenger').value;
         const message = document.getElementById('message').value;
         console.log('manager: ', manager);
         console.log('receiverId: ', receiverId);
+        console.log('messengerId: ', messengerId);
         console.log('message: ', message);
         const resultUpdate = await callMethodPromise(
             'crm.lead.update',
@@ -159,7 +181,8 @@ async function handleChatFromLead(leadId) {
                 id: leadId,
                 fields: {
                     UF_CRM_1755054223541: receiverId,
-                    UF_CRM_1755054174474: manager
+                    UF_CRM_1755054174474: manager,
+                    UF_CRM_1765866255: messengerId,
                 }
             }
         );
@@ -192,92 +215,109 @@ async function handleChatFromContact(contactId) {
     const selectElem = document.getElementById('receiver');
     selectElem.parentElement.remove();
 
-    // UF_CRM_1755055586388 - Менеджер создавший чат
     let cmd = {
         user: ['user.current', {}],
-        contact_update: [
+        fields: [`crm.contact.fields`, {}],
+        contact: [
+            'crm.contact.list',
+            {
+                filter: { ID: contactId },
+                select: [
+                    'UF_CRM_1755055586388',         // Менеджер, создавший чат
+                    'UF_CRM_694931E799E40',         // Месседжер для общения
+                ]
+            }
+        ]
+    };
+
+    const result = await callBatchPromise(cmd);
+    const user = result.user.data();
+    const fields = result.fields.data();
+    const contact = result.contact.data()?.[0];
+
+    console.log('user: ', user);
+    console.log('fields: ', fields);
+    console.log('contact: ', contact);
+
+    renderSelectField('messenger', fields?.UF_CRM_694931E799E40?.items);
+
+    document.getElementById('createChatWA').addEventListener('click', async (event) => {
+        const button = event.target;
+        button.setAttribute('disabled', '');
+        button.querySelector('span').classList.remove('d-none');
+
+        const manager = `${user?.LAST_NAME} ${user?.NAME}`;
+        const messengerId = document.getElementById('messenger').value;
+        const message = document.getElementById('message').value;
+        console.log('manager: ', manager);
+        console.log('messengerId: ', messengerId);
+        console.log('message: ', message);
+
+        const resultUpdate = await callMethodPromise(
             'crm.contact.update',
             {
                 id: contactId,
                 fields: {
-                    UF_CRM_1755055586388: '$result[user][LAST_NAME] $result[user][NAME]',
+                    UF_CRM_1755055586388: manager,
+                    UF_CRM_694931E799E40: messengerId,
                 }
             }
-        ],
-        rub_bizproc: [
+        );
+        console.log('resultUpdate = ', resultUpdate);
+
+        const resultStartBizproc = await callMethodPromise(
             'bizproc.workflow.start',
             {
                 TEMPLATE_ID: 2143,
                 DOCUMENT_ID: [
                     'crm',
                     'CCrmDocumentContact',
-                    `CONTACT_${contactId}`
-                ]
+                    `CONTACT_${contactId}`,
+                ],
+                PARAMETERS: {
+                    'Parameter1': message
+                },
             }
-        ]
-        // fields: [`crm.contact.fields`, {}],
-        // contact: [
-        //     'crm.contact.list',
-        //     {
-        //         filter: { ID: contactId },
-        //         select: [
-        //             'ID',
-        //             'UF_CRM_1755055586388',
-        //         ]
-        //     }
-        // ]
-    };
+        );
 
-    const result = callBatchPromise(cmd);
-    // const resultStartBizproc = callMethodPromise(
-    //     'bizproc.workflow.start',
-    //     {
-    //         TEMPLATE_ID: 2143,
-    //         DOCUMENT_ID: [
-    //             'crm',
-    //             'CCrmDocumentContact',
-    //             `CONTACT_${contactId}`
-    //         ]
-    //     }
-    // );
-    BX24.closeApplication();
+        console.log('resultStartBizproc = ', resultStartBizproc);
 
-    // const user = result.user.data();
-    // const fields = result.fields.data();
-    // const contact = result.contact.data()?.[0];
-    // console.log('user: ', user);
-    // console.log('contact: ', contact);
-    // document.getElementById('createChatWA').addEventListener('click', async () => {
-    //     const manager = `${user?.LAST_NAME} ${user?.NAME}`;
-    //     console.log('manager: ', manager);
-    //     const resultUpdate = await callMethodPromise(
-    //         'crm.contact.update',
-    //         {
-    //             id: contactId,
-    //             fields: {
-    //                 UF_CRM_1755055586388: manager,
-    //             }
-    //         }
-    //     );
-    //     console.log('resultUpdate = ', resultUpdate);
-    //
-    //
-    //     const resultStartBizproc = callMethodPromise(
-    //         'bizproc.workflow.start',
-    //         {
-    //             TEMPLATE_ID: 2143,
-    //             DOCUMENT_ID: [
-    //                 'crm',
-    //                 'CCrmDocumentContact',
-    //                 `CONTACT_${contactId}`
-    //             ]
-    //         }
-    //     );
-    //     console.log('resultStartBizproc = ', resultStartBizproc);
-    //
-    //     BX24.closeApplication();
-    // })
+        BX24.closeApplication();
+    })
 }
+
+
+// async function handleChatFromContact(contactId) {
+//     const selectElem = document.getElementById('receiver');
+//     selectElem.parentElement.remove();
+
+//     // UF_CRM_1755055586388 - Менеджер создавший чат
+//     let cmd = {
+//         user: ['user.current', {}],
+//         contact_update: [
+//             'crm.contact.update',
+//             {
+//                 id: contactId,
+//                 fields: {
+//                     UF_CRM_1755055586388: '$result[user][LAST_NAME] $result[user][NAME]',
+//                 }
+//             }
+//         ],
+//         rub_bizproc: [
+//             'bizproc.workflow.start',
+//             {
+//                 TEMPLATE_ID: 2143,
+//                 DOCUMENT_ID: [
+//                     'crm',
+//                     'CCrmDocumentContact',
+//                     `CONTACT_${contactId}`,
+//                 ]
+//             }
+//         ]
+//     };
+//     const result = callBatchPromise(cmd);
+//     BX24.closeApplication();
+// }
 
 
 BX24.init(async function(){
@@ -289,65 +329,4 @@ BX24.init(async function(){
     } else if (entityType === 'contact') {
         await handleChatFromContact(entityId);
     }
-
-    // let cmd = {
-    //     user: ['user.current', {}],
-    // };
-    //
-    // if (entityType === 'lead') {
-    //     // UF_CRM_1755054223541 - С кем создать чат
-    //     // UF_CRM_1755054174474 - Менеджер создавший чат
-    //     cmd['lead_field'] = [`crm.lead.fields`, {}];
-    //     cmd['lead'] = [
-    //         'crm.lead.list',
-    //         {
-    //             filter: { id: entityId },
-    //             select: [
-    //                 'ID',
-    //                 'UF_CRM_1755054223541',
-    //                 'UF_CRM_1755054174474'
-    //             ]
-    //         }
-    //     ]
-    // } else if (entityType === 'deal') {
-    //     handleChatFromDeal(entityId);
-    //     // UF_CRM_689C03C9DF905 - С кем создать чат
-    //     // UF_CRM_689C03C98BB2C - Менеджер создавший чат
-    //     // cmd['deal_field'] = `crm.deal.fields`;
-    //     // cmd['deal'] = [
-    //     //     'crm.deal.list',
-    //     //     {
-    //     //         filter: { id: entityId },
-    //     //         select: [
-    //     //             'ID',
-    //     //             'UF_CRM_689C03C9DF905',
-    //     //             'UF_CRM_689C03C98BB2C'
-    //     //         ]
-    //     //     }
-    //     // ]
-    // } else if (entityType === 'contact') {
-    //     // UF_CRM_1755055586388 - Менеджер создавший чат
-    //     cmd['contact_field'] = [`crm.contact.fields`, {}];
-    //     // cmd['contact'] = `crm.contact.get'?id=${entityId}`;
-    //     cmd['contact'] = [
-    //         'crm.contact.list',
-    //         {
-    //             filter: { id: entityId },
-    //             select: [
-    //                 'ID',
-    //                 'UF_CRM_1755055586388'
-    //             ]
-    //         }
-    //     ]
-    // }
-    // console.log('consolemd = ', cmd);
-    // const result = await callBatchPromise(cmd);
-    // console.log(result);
-
-// - Добавляем кнопку - Чат WA
-// - При нажатии, открывается небольшое окно с одним полем для заолнения
-// - Поле называется "С кем создать чат" (тут можно сослаться на готовое поле из сущности если можно)
-// - Если нет, то выбор такой: Контакт, Дизайнер
-// - В поля сущностей Нужно заполнить 2 поля: Менеджер создавший чат (сюда записываем или ID или прямо ФИО (мне лучше фио)) и с кем нужен чат
-// - Далее запускам БП под названием Создание чата WA (в обоих сухостях)
 });
